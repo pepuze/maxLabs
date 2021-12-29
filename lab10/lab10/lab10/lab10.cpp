f94ca5d5736cba83c09098c4c3929a581134cbe8
Igor_Pepuze <rolling.bbot@gmail.com>
2021-12-29 18:28:15 +0700

    lab10 - ready | lab9 - not ready

---
+++ lab10/lab10/lab10/lab10.cpp
@@ -0,0 +1,36 @@
+﻿#include <windows.h>
+#include <iostream>
+
+struct Parallelogram {
+    double a, b, angleDeg;
+};
+
+bool checkIfRectangle(Parallelogram par) {
+    if (par.angleDeg == 90) return 1;
+    return 0;
+}
+
+bool checkIfSquare(Parallelogram par) {
+    if (checkIfRectangle(par) && par.a == par.b) return 1;
+    return 0;
+}
+
+int main()
+{
+    int k, k1 = 0, k2 = 0;
+    Parallelogram pars[1000];
+    system("chcp 1251"); //Вывод кириллицы
+    std::cout << "Введите количество параллелограммов:\n";
+    std::cin >> k;
+    std::cout << "Введите стороны параллелограммов и больший угол между их сторонами (в градусах):\n";
+    for (int i = 0; i < k; ++i) std::cin >> pars[i].a >> pars[i].b >> pars[i].angleDeg;
+
+    for (int i = 0; i < k; ++i) {
+        if (checkIfRectangle(pars[i])) {
+            ++k1;
+            if (checkIfSquare(pars[i])) ++k2;
+        }
+    }
+
+    std::cout << "Количество прямоугольников: " << k1 << "\nКоличество квадратов: " << k2 << "\n";
+}
---
+++ lab9/lab9/lab9/lab9.cpp
@@ -0,0 +1,17 @@
+﻿#include <fstream>
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::string inputFilePath, outputFilePath, text, s;
+    std::cin >> inputFilePath;
+    std::ifstream fin(inputFilePath);
+    while (std::getline(fin, s)) {
+        text += s;
+    }
+    std::cout << text;
+    
+}
+
+
